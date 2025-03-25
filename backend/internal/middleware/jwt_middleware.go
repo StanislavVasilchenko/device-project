000cd22f5999646cb840bfbc2f9d17d0fe639ebb
stanislav <stanislav.vasilchenko@yandex.ru>
2025-03-25 20:44:54 +0300

    замен в meddleware на github.com/golang-jwt/jwt/v5

--- backend/internal/middleware/jwt_middleware.go
+++ backend/internal/middleware/jwt_middleware.go
@@ -2,75 +2,70 @@ package middleware
 
 import (
 	"fmt"
-	"github.com/dgrijalva/jwt-go"
-	"github.com/gin-gonic/gin"
-	"github.com/joho/godotenv"
-	"log"
 	"net/http"
 	"os"
 	"strings"
+
+	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v5"
 )
 
 type Claims struct {
 	Username string `json:"username"`
-	jwt.StandardClaims
+	jwt.RegisteredClaims
 }
 
 func JwtMiddleware() gin.HandlerFunc {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatalf("Error loading .env file: %v", err)
-	}
-	var jwtKey = []byte(os.Getenv("SECRET_JWT_KEY"))
-
 	return func(c *gin.Context) {
-		var err = godotenv.Load()
-		if err != nil {
-			log.Fatalf("Error loading .env file: %v", err)
-		}
-		// Получаем заголовок Authorization
-		authHeader := c.GetHeader("Authorization")
-
-		if authHeader == "" {
-			c.JSON(http.StatusUnauthorized, gin.H{"error": "Authorization header is required"})
-			c.Abort()
+		jwtKey := os.Getenv("JWT_SECRET")
+		if jwtKey == "" {
+			c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{
+				"error": "JWT secret key not configured",
+			})
 			return
 		}
 
-		// Удаляем префикс "Bearer " из заголовка
-		tokenString := strings.TrimPrefix(authHeader, "Bearer ")
+		authHeader := c.GetHeader("Authorization")
+		if authHeader == "" {
+			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{
+				"error": "Authorization header is required",
+			})
+			return
+		}
 
+		tokenString := strings.TrimPrefix(authHeader, "Bearer ")
 		if tokenString == "" {
-			c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid token format"})
-			c.Abort()
+			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{
+				"error": "Invalid token format",
+			})
 			return
 		}
 
 		claims := &Claims{}
-
-		// Парсим токен
 		token, err := jwt.ParseWithClaims(tokenString, claims, func(token *jwt.Token) (interface{}, error) {
-			// Проверяем, что используется правильный алгоритм
+
 			if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
 				return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
 			}
-			return jwtKey, nil
+			return []byte(jwtKey), nil
 		})
 
 		if err != nil {
-			fmt.Println("Error parsing token:", err)
-			c.JSON(http.StatusUnauthorized, gin.H{"error": "Unauthorized"})
-			c.Abort()
+			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{
+				"error":   "Invalid token",
+				"details": err.Error(),
+			})
 			return
 		}
 
 		if !token.Valid {
-			fmt.Println("Invalid token")
-			c.JSON(http.StatusUnauthorized, gin.H{"error": "Unauthorized"})
-			c.Abort()
+			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{
+				"error": "Invalid token",
+			})
 			return
 		}
 
+		c.Set("claims", claims)
 		c.Next()
 	}
 }
